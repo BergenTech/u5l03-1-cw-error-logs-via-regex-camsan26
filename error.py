b061faf532f730a9857d2aedf8b4b24f51cbdf16
Camila <camsan26@bergen.org>
2024-03-12 09:04:08 -0400

    incomplete (almost finished)

--- error.py
+++ error.py
@@ -1,1 +1,18 @@
-with open()
+import json, csv, re
+file_path = "logs.json"
+with open(file_path,'r') as file:
+    data = json.load(file)
+    pattern = r'ERROR'
+    matching = []
+    for i in data:
+        match = re.search(pattern,i['level'],re.IGNORECASE)
+        # print(match)
+        matching.append(i)
+    # print(matching)
+with open('logs.csv','w',newline='') as CSV_file:
+    fieldnames = ['timestamp','level','message']
+    writer = csv.DictWriter(CSV_file,fieldnames=fieldnames)
+    writer.writeheader()
+    for i in data:
+        writer.writerow(i['timestamp'],i['level'],i['message'])
+    
